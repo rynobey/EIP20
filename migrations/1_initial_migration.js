f53866296cf094a6b432db8eda4dad4791004ad3
Ryno Beyers <rynobey@gmail.com>
2018-01-12 15:59:30 +0200

    Added EIP20 to migration script

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var EIP20 = artifacts.require("./EIP20.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(EIP20, 10000, "Training Bucks", 0, "TBX");
 };
